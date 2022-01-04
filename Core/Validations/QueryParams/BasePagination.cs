ccaa6b44773bc92932b31abe289a276e475e5d0c
Oscar Chavez <oscarchb04@gmail.com>
2022-01-04 14:40:24 -0400

    *Add some folders and code to Validations

---
+++ Core/Validations/QueryParams/BasePagination.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Validations.QueryParams
+{
+    /// <summary>
+    /// Basic class to paginations query params
+    /// </summary>
+    public class BasePagination
+    {
+        /// <summary>
+        /// Page number to query =>  1 is by default
+        /// </summary>
+        public int PageNumber { get; set; } = 1;
+    }
+}
